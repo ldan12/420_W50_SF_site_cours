4922c31d9b46b69f39359a6712c93137e882248e
ldan12 <ldan12@hotmail.com>
2026-08-17 10:57:41 -0400

    Typo in deploy workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://https://ldan12.github.io',
+  url: 'https://ldan12.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/420-W50-SF-notes-cours/',
